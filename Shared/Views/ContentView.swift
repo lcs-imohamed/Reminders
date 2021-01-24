90042674f990a028a5464b2dd19e9bb09d248908
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-01-24 17:10:02 -0500

    Connected Content View to AddTask view using sheet view modifier

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     
+    //Stores all tasks that are being tacked
     @ObservedObject var store: TaskStore
+    
+    // controls whether the add task is showing
+    @State private var showingAddTask = false
+    
     
     var body: some View {
         List(store.tasks) { task in
             TaskCell(task: task)
         }
         .navigationTitle("Reminders")
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button("Add") {
+                    showingAddTask = true
+                }
+            }
+        }
+        .sheet(isPresented: $showingAddTask) {
+            AddTask(showing: $showingAddTask)
+        }
     }
 }
 
@@ -24,6 +39,6 @@ struct ContentView_Previews: PreviewProvider {
         NavigationView {
             ContentView(store: testStore)
         }
-       
+        
     }
 }

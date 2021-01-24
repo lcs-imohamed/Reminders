81c4da3914b09ecd0388d99251fc0b20ae235fa1
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-01-24 13:18:53 -0500

    Initial Commit

---
+++ Shared/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Shared
+//
+//  Created by Mohamed, Ilana on 2021-01-24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
